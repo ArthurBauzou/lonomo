bb94c759b5b6f3927b3e7c39618e7182bbf84371
KevChausse <chaussekevin@gmail.com>
2017-08-23 17:29:37 +0200

    tri du resultat de l'api google maps et retour json

--- routes/apis.js
+++ routes/apis.js
@@ -14,15 +14,37 @@ router.post('/', function(req, res, next) {
     depart = depart.replace(/\s/g, '+');
     arriv = arriv.replace(/\s/g, '+');
 
-    console.log('depart : '+depart+' - arrivee : '+arriv);
-
     request('https://maps.googleapis.com/maps/api/directions/json?mode='+mode+'&origin='+depart+'&destination='+arriv+'&language=fr&key=AIzaSyBy94XeHduKyseqtx3gu9tHCQXwBz9qvG8', function (error, response, body) {
         var parsej = JSON.parse(body);
 
-        
+        //var results = parsej;
+        var results = {};
 
+        if(parsej["routes"][0]){
 
-        res.send(parsej);
+            results["distance"] = parsej["routes"][0]["legs"][0]["distance"];
+            results["duration"] = parsej["routes"][0]["legs"][0]["duration"];
+            results["end_address"] = parsej["routes"][0]["legs"][0]["end_address"];
+            results["end_location"] = parsej["routes"][0]["legs"][0]["end_location"];
+            results["start_address"] = parsej["routes"][0]["legs"][0]["start_address"];
+            results["start_location"] = parsej["routes"][0]["legs"][0]["start_location"];
+
+            if(mode == "transit"){
+                results["arrival_time"] = parsej["routes"][0]["legs"][0]["arrival_time"];
+                results["departure_time"] = parsej["routes"][0]["legs"][0]["departure_time"];
+                results["tarifs"] = parsej["routes"][0]["fare"]["text"];
+            }
+
+            if(mode == "driving"){
+                results["tarifs"] = results["distance"].value/1000*0.1 //calcul approximatif du cout en carburant
+            }
+
+        }
+        else {
+            results["no_results"] = "Aucun resultat disponible pour ce moyen de transport";
+        }
+
+        res.send(results);
     });
 
 });
