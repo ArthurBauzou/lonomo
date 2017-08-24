182791476f6a50fa1065e49069873706a03aa23a
arthurbauzou <arthurbauzou@gmail.com>
2017-08-24 12:11:34 +0200

    viable results page

--- routes/results.js
+++ routes/results.js
@@ -2,7 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('results', { title: 'Lonomo' });
+  res.render('results', {
+    title: 'Lonomo',
+    tags: [
+      {
+        duration: '2h15',
+        distance: 12.3,
+        walking: true
+      },
+      {
+        duration: '0h53',
+        distance: 13.9,        
+        bicycle: true
+      },
+      {
+        duration: '0h15',
+        distance: 14.0,        
+        transit: true
+      }
+    ]
+  });
 });
 
 module.exports = router;
