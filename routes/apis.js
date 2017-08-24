cda42c59aaa41e759e26e4a911bdad0fae5e6bc4
KevChausse <chaussekevin@gmail.com>
2017-08-24 17:27:59 +0200

    gestion des modes de transport de l'api interne - prototype

--- routes/apis.js
+++ routes/apis.js
@@ -10,83 +10,104 @@ router.post('/', function(req, res, next) {
     var depart = req.body.departure;
     var arriv = req.body.arrival;
     var mode = req.body.mode;
+  /*  var walking = req.body.walking;
+    var driving = req.body.driving;
+    var bicycling = req.body.bicycling;
+    var transit = req.body.transit;
+*/
+
     var parsej ="";
     var results = {};
 
     depart = depart.replace(/\s/g, '+');
     arriv = arriv.replace(/\s/g, '+');
 
-    request('https://maps.googleapis.com/maps/api/directions/json?mode='+mode+'&origin='+depart+'&destination='+arriv+'&language=fr&key=AIzaSyBy94XeHduKyseqtx3gu9tHCQXwBz9qvG8', function (error, response, body) {
-        parsej = JSON.parse(body);
+   // var reqApi = function(mode){
 
-        var results = parsej;
-        var results = {};
+        request('https://maps.googleapis.com/maps/api/directions/json?mode='+mode+'&origin='+depart+'&destination='+arriv+'&language=fr&key=AIzaSyBy94XeHduKyseqtx3gu9tHCQXwBz9qvG8', function (error, response, body) {
+            parsej = JSON.parse(body);
 
-        if(parsej["routes"][0]){
+            var results = parsej;
+            var results = {};
 
-            results["distance"] = parsej["routes"][0]["legs"][0]["distance"];
-            results["duration"] = parsej["routes"][0]["legs"][0]["duration"];
-            results["end_address"] = parsej["routes"][0]["legs"][0]["end_address"];
-            results["start_address"] = parsej["routes"][0]["legs"][0]["start_address"];
-            results["end_location"] = parsej["routes"][0]["legs"][0]["end_location"];
-            results["start_location"] = parsej["routes"][0]["legs"][0]["start_location"];
-            
+            if(parsej["routes"][0]){
 
-            if(mode == "transit"){
-                results["arrival_time"] = parsej["routes"][0]["legs"][0]["arrival_time"];
-                results["departure_time"] = parsej["routes"][0]["legs"][0]["departure_time"];
-                results["tarifs"] = parsej["routes"][0]["fare"]["text"];
-            }
+                results["distance"] = parsej["routes"][0]["legs"][0]["distance"];
+                results["duration"] = parsej["routes"][0]["legs"][0]["duration"];
+                results["end_address"] = parsej["routes"][0]["legs"][0]["end_address"];
+                results["start_address"] = parsej["routes"][0]["legs"][0]["start_address"];
+                results["end_location"] = parsej["routes"][0]["legs"][0]["end_location"];
+                results["start_location"] = parsej["routes"][0]["legs"][0]["start_location"];
 
-            if(mode == "driving"){
-                results["tarifs"] = (results["distance"].value/1000*0.11).toFixed(2) //calcul approximatif du cout en carburant
-            }
-
-            var altFunc = function(retFunc){
-              if(mode=="walking"||mode=="bicycling"){
-                    results["altitude"] = {};
-                    var coords = [];
-                    var altFunc;
-                    var url=results["start_location"]["lat"]+","+results["start_location"]["lng"];
-                    
-                    coords = parsej["routes"][0]["legs"][0]["steps"]
-                    
-                    for(var elt =0; elt<coords.length;elt++){
-                        url += "|"+coords[elt]["end_location"]["lat"]+","+coords[elt]["end_location"]["lng"]
-                    }
-                                        
+                if(mode == "bicycling") results["bicycling"] = true;
+                if(mode == "walking") results["walking"] = true;
                 
-                    request('https://maps.googleapis.com/maps/api/elevation/json?locations='+url+'&key=AIzaSyBy94XeHduKyseqtx3gu9tHCQXwBz9qvG8', function (error, response, body_elev){
-                        var parselev = JSON.parse(body_elev);
 
-                        parselev = parselev["results"];
-                        elevation = 0;
+                if(mode == "transit"){
+                    results["transit"] = true;
+                    results["arrival_time"] = parsej["routes"][0]["legs"][0]["arrival_time"];
+                    results["departure_time"] = parsej["routes"][0]["legs"][0]["departure_time"];
 
-                        for(var elt = 1; elt<parselev.length;elt++){
-                            tmpElev = parselev[elt]["elevation"]-parselev[elt-1]["elevation"];
-                            if( tmpElev > 0 ){
-                                elevation += tmpElev;
-                            }
-                        }
-                        
-                        retFunc(elevation);
-                    })
-                
+                    if(parsej["routes"][0]["fare"]) results["tarifs"] = parsej["routes"][0]["fare"]["text"];
+                    else results["tarifs"] = "0,00€"
                 }
-                else retFunc(0);
+
+                if(mode == "driving"){
+                    results["driving"] = true;
+                    results["tarifs"] = (results["distance"].value/1000*0.11).toFixed(2) //calcul approximatif du cout en carburant
+                }
+
+                var altFunc = function(retFunc){
+                if(mode=="walking"||mode=="bicycling"){
+                        results["altitude"] = {};
+                        var coords = [];
+                        var altFunc;
+                        var url=results["start_location"]["lat"]+","+results["start_location"]["lng"];
+                        
+                        coords = parsej["routes"][0]["legs"][0]["steps"]
+                        
+                        for(var elt =0; elt<coords.length;elt++){
+                            url += "|"+coords[elt]["end_location"]["lat"]+","+coords[elt]["end_location"]["lng"]
+                        }
+                                            
+                    
+                        request('https://maps.googleapis.com/maps/api/elevation/json?locations='+url+'&key=AIzaSyBy94XeHduKyseqtx3gu9tHCQXwBz9qvG8', function (error, response, body_elev){
+                            var parselev = JSON.parse(body_elev);
+
+                            parselev = parselev["results"];
+                            elevation = 0;
+
+                            for(var elt = 1; elt<parselev.length;elt++){
+                                tmpElev = parselev[elt]["elevation"]-parselev[elt-1]["elevation"];
+                                if( tmpElev > 0 ){
+                                    elevation += tmpElev;
+                                }
+                            }
+                            
+                            retFunc(elevation);
+                        })
+                    
+                    }
+                    else retFunc(0);
+                }
+
+            }
+            else {
+                results["no_results"] = "Aucun resultat disponible pour ce moyen de transport";
             }
 
-        }
-        else {
-            results["no_results"] = "Aucun resultat disponible pour ce moyen de transport";
-        }
+            altFunc(function(resultat){
+                    results["altitude"] = resultat;
+                    //return results;
+                    res.send(results)
+            });
 
-        altFunc(function(resultat){
-                results["altitude"] = resultat;
-                res.send(results);
         });
 
-    });
+
+
+
+ //   }
 
 });
 
