6943d4bd20b4569e2b242a7d973697643d295b59
arthurbauzou <arthurbauzou@gmail.com>
2017-08-25 15:19:35 +0200

    search badly functionnal

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,11 +3,15 @@
 let modeBtns = document.querySelectorAll('button[type="button"]')
 modeBtns.forEach((el)=> {
   el.addEventListener('click', ()=> {
-    console.log(el)
+
     if (el.className === 'btnActive') {
       el.setAttribute('class', 'btnInactive')
+      document.querySelector("input[name='"+el.getAttribute("name")+"']").checked = false
+      console.log(document.querySelector("input[name='"+el.getAttribute("name")+"']"))
     } else {
       el.setAttribute('class', 'btnActive')
+      document.querySelector("input[name='"+el.getAttribute("name")+"']").checked = true
+
     }
   })
 });
@@ -34,30 +38,33 @@ form.addEventListener('submit', (e)=>{
 
   // request
   if (departure.value !== '' && arrival.value !== '') {
-    let request = {
-      "departure": departure.value,
-      "arrival": arrival.value
-    }
+    // let request = {
+    //   "departure": departure.value,
+    //   "arrival": arrival.value
+    // }
 
-    modeBtns.forEach((el)=> {
-      let modeName = el.getAttribute("name")
-      console.log(modeName)
-      if (el.className === 'btnActive') {
-        request[modeName] = true
-      } else {
-        request[modeName] = false
-      }
-    })
+    // modeBtns.forEach((el)=> {
+    //   let modeName = el.getAttribute("name")
+    //   console.log(modeName)
+    //   if (el.className === 'btnActive') {
+    //     request[modeName] = true
+    //   } else {
+    //     request[modeName] = false
+    //   }
+    // })
 
-    console.log(request)
+    // console.log(request)
 
-    var data = new FormData();
-    data.append("json", JSON.stringify(request));
+    form.submit();
 
-    fetch("/apis", {
-      method: "POST",
-      body: data
-    })
+    // fetch("/results", {
+    //   method: "POST",
+    //   body: JSON.stringify(request),
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json'
+    //   }
+    // })
   }
 
 })
--- routes/results.js
+++ routes/results.js
@@ -1,7 +1,29 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var request = require('request')
+
+// var bodyParser = require('body-parser');
+// var app = express();
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+router.post('/', (req, res, next)=> {
+
+  console.log(req.body)
+
+  request.post({url:"http://localhost:3000/apis", form: req.body}, (err, httpResponse, data)=> {
+    superdata = {"tags": JSON.parse(data)}
+    superdata.title = 'results'
+    console.log(superdata)
+
+    res.render('results', superdata)
+    console.log('ici c paris')
+  })
+  
+})
 
 router.get('/', function(req, res, next) {
+  
   res.render('results', {
     title: 'Lonomo',
     tags: [
@@ -24,4 +46,4 @@ router.get('/', function(req, res, next) {
   });
 });
 
-module.exports = router;
+module.exports = router
